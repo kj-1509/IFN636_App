55e9dbe74a678d118d852a9341b0b0551b89782b
Kelsey <kelseypng@gmail.com>
2026-03-28 07:00:39 +0000

    home and navabr

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home'
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import Tasks from './pages/Tasks';
+import { AuthProvider } from './context/AuthContext';
+import './App.css'
 
 function App() {
   return (
+  <AuthProvider>
     <Router>
       <Navbar />
       <Routes>
+        <Route path ="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/tasks" element={<Tasks />} />
       </Routes>
     </Router>
-  );
+  </AuthProvider>
+ );
 }
 
 export default App;
